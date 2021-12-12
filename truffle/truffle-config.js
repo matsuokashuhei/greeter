3a4653bbe2cded280bb23ae7065e8f7d84f2d4b9
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2021-12-12 20:55:55 +0900

    Fix

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -46,11 +46,6 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    TextDecoderStream: {
-     host: "ganache",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
