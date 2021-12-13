616d56379393a4628aba7b4707cd330832c2d424
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2021-12-14 00:14:17 +0900

    :construction: Continue to learn

--- truffle/contracts/Greeter.sol
+++ truffle/contracts/Greeter.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.8.10;
 
-contract Greeter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function greet() external view returns (string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+        _greeting = greeting;
+    }
 }
